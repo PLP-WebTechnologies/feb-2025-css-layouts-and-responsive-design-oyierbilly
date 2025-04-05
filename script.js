853b8bb635e9e8616abcf7794d4ab24bf73d4e67
Billy Oyier <oyierbilly01@gmail.com>
2025-04-05 11:21:59 +0300

    Assignment submitted

---
+++ script.js
@@ -0,0 +1,11 @@
+// Mobile menu toggle
+document.querySelector('.hamburger').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
+
+// Close mobile menu when clicking on a link
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.querySelector('.nav-links').classList.remove('active');
+    });
+});
